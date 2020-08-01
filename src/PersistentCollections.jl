719045836948edde5a9eb5e4910914d279b9285f
Zalan Blenessy <zalan.blenessy@gmail.com>
2020-08-01 19:02:24 +0200

    read(::Environment), write(::Environment)  works
    With methods:
    - put!
    - get
    - delete!

---
+++ src/PersistentCollections.jl
@@ -0,0 +1,282 @@
+module PersistentCollections
+    module LMDB
+        using LMDB_jll
+
+        # Error codes (I care about)
+        const MDB_SUCCESS = Cint(0)
+        const MDB_NOTFOUND = Cint(-30798)
+        
+        # Environment flags (I care about)
+        const MDB_NOSUBDIR = Cuint(0x4000)
+        const MDB_RDONLY = Cuint(0x20000)
+        const MDB_NOTLS = Cuint(0x200000)
+
+        const Cmode_t = Cushort
+
+        macro fieldoffset(T, num)
+            return :(fieldoffset($T, $(esc(num))))
+        end
+
+        # mutable struct MDBValue{T}
+        #     size::Csize_t    # size of the data item
+        #     ptr::Ptr{Cvoid}  # address of the data item
+        #     ref::T           # keep a reference to the object we are pointing to
+        #     MDBValue(size, ptr, ref::T) where {T} = new{T}(size, ptr, ref)
+        # end
+
+        mutable struct MDBValue{T}
+            size::Csize_t    # size of the data item
+            ptr::Ptr{Cvoid}  # address of the data item
+            data::T
+            MDBValue() = new{Nothing}(zero(Csize_t), C_NULL)
+            MDBValue(v::String) = new{String}(sizeof(v), pointer(v), v)
+            MDBValue(v::Array) = new{Array}(sizeof(v), pointer(v), v)
+            function MDBValue(v::T) where {T}
+                # alloc new object; set size; copy primitive value or reference
+                o = new{T}(sizeof(v), C_NULL, v)
+                # mutable struct have static pointer address so its safe to just return that
+                # primitive types and immutable structs have been copied into o - point to that instead
+                o.ptr = ismutable(v) ? pointer_from_objref(v) : pointer_from_objref(o) + @fieldoffset(MDBValue{0}, 3)
+                return o
+            end
+        end
+
+        # MDBValue (output) -> T
+        Base.convert(::Type{String}, val::MDBValue{Nothing}) = unsafe_string(convert(Ptr{UInt8}, val.ptr), val.size)
+        Base.convert(::Type{Array{T,N}}, val::MDBValue{Nothing}) where {T,N} = unsafe_wrap(Array, convert(Ptr{T}, val.ptr), val.size)
+        Base.convert(::Type{T}, val::MDBValue{Nothing}) where {T} = unsafe_load(convert(Ptr{T}, val.ptr))
+        Base.convert(::Type{Any}, val::MDBValue{Nothing}) = val
+
+        # T -> MDBValue (input)
+        Base.convert(::Type{MDBValue}, val::MDBValue) = val
+        Base.convert(::Type{MDBValue}, val) = MDBValue(val)
+        Base.convert(::Type{T}, val::MDBValue{T}) where {T} = val.data
+
+        #Base.convert(::Type{String}, val::MDBValue) = unsafe_string(convert(Ptr{UInt8}, val.ptr), val.size)
+        #Base.convert(::Type{Array{T,N}}, val::MDBValue) where {T,N} = reinterpret(T, unsafe_wrap(Array, convert(Ptr{UInt8}, val.ptr), val.size))
+        #Base.convert(::Type{T}, val::MDBValue) where {T} = unsafe_load(convert(Ptr{T}, val.ptr))
+        
+        struct LMDBError <: Exception
+            code::Cint
+        end
+        Base.show(io::IO, err::LMDBError) = print(io, "$(mdb_strerror(err.code)) ($(err.code))")
+
+        macro chkres(res)
+            return :(iszero($(esc(res))) || throw(LMDBError($(esc(res)))))
+        end
+
+        function mdb_strerror(err::Cint)
+            errstr = ccall((:mdb_strerror, liblmdb), Cstring, (Cint,), err)
+            return unsafe_string(errstr)
+        end
+
+        function mdb_env_create()
+            env_ref = Ref{Ptr{Cvoid}}(C_NULL)
+            @chkres ccall((:mdb_env_create, liblmdb), Cint, (Ptr{Ptr{Cvoid}},), env_ref)
+            return env_ref[]
+        end
+
+        function mdb_env_close(env::Ptr{Cvoid})
+            ccall((:mdb_env_close, liblmdb), Cvoid, (Ptr{Cvoid},), env)
+            return nothing
+        end
+
+        function mdb_env_open(env::Ptr{Cvoid}, path::String, flags::Cuint, mode::Cmode_t)
+            @chkres ccall((:mdb_env_open, liblmdb), Cint, (Ptr{Cvoid}, Cstring, Cuint, Cmode_t), env, path, flags, mode)
+            return nothing
+        end
+
+        function mdb_version()
+            str = ccall((:mdb_version, liblmdb), Cstring, (Ptr{Cint}, Ptr{Cint}, Ptr{Cint}), C_NULL, C_NULL, C_NULL)
+            return unsafe_string(str)
+        end
+
+        function mdb_txn_begin(env::Ptr{Cvoid}, flags::Cuint)
+            txn_ref = Ref{Ptr{Cvoid}}(C_NULL)
+            @chkres ccall((:mdb_txn_begin, liblmdb), Cint, (Ptr{Cvoid}, Ptr{Cvoid}, Cuint, Ptr{Ptr{Cvoid}}), env, C_NULL, flags, txn_ref)
+            return txn_ref[]
+        end
+
+        function mdb_txn_abort(txn::Ptr{Cvoid})
+            ccall((:mdb_txn_abort, liblmdb), Cvoid, (Ptr{Cvoid},), txn)
+            return nothing
+        end
+
+        function mdb_txn_reset(txn::Ptr{Cvoid})
+            ccall((:mdb_txn_reset, liblmdb), Cvoid, (Ptr{Cvoid},), txn)
+            return nothing
+        end
+
+        function mdb_txn_renew(txn::Ptr{Cvoid})
+            @chkres ccall((:mdb_txn_renew, liblmdb), Cint, (Ptr{Cvoid},), txn)
+            return nothing
+        end
+
+        function mdb_txn_commit(txn::Ptr{Cvoid})
+            @chkres ccall((:mdb_txn_commit, liblmdb), Cint, (Ptr{Cvoid},), txn)
+            return
+        end
+
+        function mdb_dbi_open(txn::Ptr{Cvoid}, dbname::String, flags::Cuint)
+            handle = Cuint[0]
+            cdbname = isempty(dbname) ? convert(Cstring, Ptr{UInt8}(C_NULL)) : dbname
+            @chkres ccall((:mdb_dbi_open, liblmdb), Cint, (Ptr{Cvoid}, Cstring, Cuint, Ptr{Cuint}), txn, cdbname, flags, handle)
+            return handle[1]
+        end
+
+        function mdb_dbi_close(txn::Ptr{Cvoid}, dbi::Cuint)
+            ccall((:mdb_dbi_close, liblmdb), Cvoid, (Ptr{Cvoid}, Cuint), txn, dbi)
+            return nothing
+        end        
+
+        function mdb_env_set_maxreaders(env::Ptr{Cvoid}, readers::Cuint)
+            @chkres ccall((:mdb_env_set_maxreaders, liblmdb), Cint, (Ptr{Cvoid}, Cuint), env, readers)
+            return nothing
+        end
+
+        function mdb_env_set_mapsize(env::Ptr{Cvoid}, size::Csize_t)
+            @chkres ccall((:mdb_env_set_mapsize, liblmdb), Cint, (Ptr{Cvoid}, Csize_t), env, size)
+            return nothing
+        end
+
+        function mdb_env_set_maxdbs(env::Ptr{Cvoid}, dbs::Cuint)
+            @chkres ccall((:mdb_env_set_maxdbs, liblmdb), Cint, (Ptr{Cvoid}, Cuint), env, dbs)
+            return nothing
+        end
+
+        function mdb_put(txn::Ptr{Cvoid}, dbi::Cuint, key::MDBValue, val::MDBValue, flags::Cuint)
+            keyp = pointer_from_objref(key)
+            valp = pointer_from_objref(val)
+            @chkres ccall((:mdb_put, liblmdb), Cint, (Ptr{Cvoid}, Cuint, Ptr{Cvoid}, Ptr{Cvoid}, Cuint), txn, dbi, keyp, valp, flags)
+            return nothing
+        end        
+
+        function mdb_del(txn::Ptr{Cvoid}, dbi::Cuint, key::MDBValue, val::Union{MDBValue,Nothing})
+            keyp = pointer_from_objref(key)
+            valp = isnothing(val) ? C_NULL : pointer_from_objref(val)
+            res = ccall((:mdb_del, liblmdb), Cint, (Ptr{Cvoid}, Cuint, Ptr{Cvoid}, Ptr{Cvoid}), txn, dbi, keyp, valp)
+            (res == MDB_SUCCESS || res == MDB_NOTFOUND) || throw(LMDBError(res))
+            return res != MDB_NOTFOUND
+        end        
+
+        function mdb_get(txn::Ptr{Cvoid}, dbi::Cuint, key::MDBValue, val::MDBValue)
+            keyp = pointer_from_objref(key)
+            valp = pointer_from_objref(val)
+            res = ccall((:mdb_get, liblmdb), Cint, (Ptr{Cvoid}, Cuint, Ptr{Cvoid}, Ptr{Cvoid}), txn, dbi, keyp, valp)
+            (res == MDB_SUCCESS || res == MDB_NOTFOUND) || throw(LMDBError(res))
+            return res != MDB_NOTFOUND
+        end        
+
+        function __init__()
+            @info mdb_version()
+            # this will catch changes in struct layout
+            @assert fieldname(MDBValue{Nothing}, 3) == :data
+        end
+    end
+
+    mutable struct Environment
+        handle::Ptr{Cvoid}
+        path::String
+        maxdbs::Cuint
+        rotxn::Vector{Ptr{Cvoid}}
+    end
+
+    const OPENED_ENVS = Dict{String,Environment}()
+
+    function Environment(path::String; flags::Cuint=zero(Cuint), mode::LMDB.Cmode_t = 0o755, maxdbs::Cuint = zero(Cuint), mapsize::Csize_t = Csize_t(10485760),
+                                       maxreaders::Cuint = Cuint(126), rotxnflags::Cuint = (LMDB.MDB_RDONLY | LMDB.MDB_NOTLS))
+        env = get(OPENED_ENVS, path, nothing)
+        if isnothing(env)
+            rotxn = [C_NULL for i in 1:Threads.nthreads()]
+            env = Environment(LMDB.mdb_env_create(), path, maxdbs, rotxn)
+            LMDB.mdb_env_set_maxdbs(env.handle, maxdbs)
+            LMDB.mdb_env_set_mapsize(env.handle, mapsize)
+            LMDB.mdb_env_set_maxreaders(env.handle, maxreaders)
+            try
+                LMDB.mdb_env_open(env.handle, path, flags, mode)
+            catch e
+                LMDB.mdb_env_close(env.handle)
+                rethrow(e)
+            end
+            OPENED_ENVS[path] = env
+            # create read-only transaction handles, which can be used quickly (mdb_txn_renew/mdb_txn_reset use-case)
+            for i in 1:Threads.nthreads()
+                env.rotxn[i] = LMDB.mdb_txn_begin(env.handle, rotxnflags)
+                LMDB.mdb_txn_reset(env.rotxn[i])
+            end
+            # register finalizer for lazy cleanup
+            finalizer(close, env)
+        end
+        return env
+    end
+
+    Base.isopen(env::Environment) = env.handle != C_NULL
+
+    function Base.close(env::Environment)
+        if isopen(env)
+            for handle in env.rotxn
+                handle == C_NULL || LMDB.mdb_txn_abort(handle)
+            end
+            @assert get(OPENED_ENVS, env.path, nothing) == env "OPENED_ENVS does not contain env for $(path) - smells like threading issue ..."
+            LMDB.mdb_env_close(env.handle)
+            env.handle = C_NULL
+            delete!(OPENED_ENVS, env.path)
+            return true
+        end
+        return false
+    end
+
+    function Base.write(func::Function, env::Environment; dbname="", txnflags::Cuint=zero(Cuint), dbiflags::Cuint=zero(Cuint))
+        isopen(env) || error("Environment is closed")
+        txn = LMDB.mdb_txn_begin(env.handle, txnflags)
+        commit = false
+        try
+            dbi = LMDB.mdb_dbi_open(txn, dbname, dbiflags)
+            func(txn, dbi)
+            LMDB.mdb_txn_commit(txn)
+            commit = true
+        finally
+            commit || LMDB.mdb_txn_abort(txn)
+        end
+    end
+
+    mutable struct Transaction
+        txn::Ptr{Cvoid}
+        dbi::Cuint
+    end
+
+    function Base.read(func::Function, env::Environment; dbname="", dbiflags::Cuint=zero(Cuint))
+        isopen(env) || error("Environment is closed")
+        txn = env.rotxn[Threads.threadid()]
+        LMDB.mdb_txn_renew(txn)
+        try
+            dbi = LMDB.mdb_dbi_open(txn, dbname, dbiflags)
+            func(txn, dbi)
+        finally
+            LMDB.mdb_txn_reset(txn)
+        end
+    end
+        
+    function Base.put!(txn::Ptr{Cvoid}, dbi::Cuint, key, val; flags=zero(Cuint))
+        @assert txn != C_NULL && !iszero(dbi) "txn and/or dbi handles are not initialized"
+        mdbkey = convert(LMDB.MDBValue, key)
+        mdbval = convert(LMDB.MDBValue, val)
+        GC.@preserve mdbkey mdbval LMDB.mdb_put(txn, dbi, mdbkey, mdbval, flags)
+        return nothing
+    end
+
+    function Base.delete!(txn::Ptr{Cvoid}, dbi::Cuint, key)
+        @assert txn != C_NULL && !iszero(dbi) "txn and/or dbi handles are not initialized"
+        mdbkey = convert(LMDB.MDBValue, key)
+        return GC.@preserve mdbkey LMDB.mdb_del(txn, dbi, mdbkey, nothing)
+    end
+
+    function Base.get(txn::Ptr{Cvoid}, dbi::Cuint, key, default::V) where {V}
+        @assert txn != C_NULL && !iszero(dbi) "txn and/or dbi handles are not initialized"
+        mdbkey = convert(LMDB.MDBValue, key)
+        mdbval = LMDB.MDBValue()
+        found = GC.@preserve mdbkey LMDB.mdb_get(txn, dbi, mdbkey, mdbval)
+        return found ? convert(V, mdbval) : default
+    end
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,102 @@
+using Test
+using BenchmarkTools
+
+using PersistentCollections: Environment, LMDB
+
+const TEST_DIR="test.lmdb"
+# clean previous runs
+rm(TEST_DIR, force=true, recursive=true)
+mkdir(TEST_DIR)
+
+@test string(LMDB.LMDBError(-30798)) == "MDB_NOTFOUND: No matching key/data pair found (-30798)" 
+
+env = Environment(TEST_DIR)
+@test isopen(env)
+@test env == Environment(TEST_DIR)
+@test close(env)
+@test !isopen(env)
+@test !close(env) # nothing bad should happen
+
+env = Environment(TEST_DIR)
+
+struct Immutable
+    a::Int
+    b::Float64
+    c::Tuple{Int,Float64}
+end
+Base.:(==)(a::Immutable, b::Immutable) = a.a == b.a && a.b == b.b && a.c == b.c
+
+mutable struct Mutable
+    a::Int
+    b::Float64
+    c::Tuple{Int,Float64}
+end
+Base.:(==)(a::Mutable, b::Mutable) = a.a == b.a && a.b == b.b && a.c == b.c
+
+ro = Immutable(1, 2.5, (2, 3.4))
+@assert !ismutable(ro)
+
+rw = Mutable(1, 2.5, (2, 3.4))
+@assert ismutable(rw)
+
+fastkey = LMDB.MDBValue("fastkey")
+fastval = LMDB.MDBValue("fastval")
+
+write(env) do txn, dbi
+    put!(txn, dbi, fastkey, fastval)
+    put!(txn, dbi, "stringkey", "stringval")
+    put!(txn, dbi, Vector{UInt8}("byteskey"), Vector{UInt8}("bytesval"))
+    put!(txn, dbi, "intkey", 1234)
+    put!(txn, dbi, "floatkey", 2.5)
+    put!(txn, dbi, "tuplekey", (1, 2.5))
+    put!(txn, dbi, "immutable_struct_key", ro)
+    put!(txn, dbi, "mutable_struct_key", rw)
+    # Set use-case
+    put!(txn, dbi, "nothing_key", nothing)
+end
+
+read(env) do txn, dbi
+    @test get(txn, dbi, fastkey, "") == "fastval"
+    @test get(txn, dbi, "stringkey", "") == "stringval"
+    @test get(txn, dbi, Vector{UInt8}("byteskey"), UInt8[]) == Vector{UInt8}("bytesval")
+    @test get(txn, dbi, "intkey", 0) == 1234
+    @test get(txn, dbi, "floatkey", 0.0) == 2.5
+    @test get(txn, dbi, "tuplekey", (0, 0.0)) == (1, 2.5)
+    @test get(txn, dbi, "immutable_struct_key", Immutable(0, 0.0, (0, 0.0))) == ro
+    @test get(txn, dbi, "immutable_struct_key", Mutable(0, 0.0, (0, 0.0))) == rw
+    # Set use-case
+    notfound = UInt8[0x1]
+    @test get(txn, dbi, "nothing_key", notfound) != notfound
+end
+
+# delete
+write(env) do txn, dbi
+    @test delete!(txn, dbi, fastkey)
+    @test delete!(txn, dbi, "stringkey")
+    # idempotency
+    @test !delete!(txn, dbi, "stringkey")
+end
+
+# check deleted
+read(env) do txn, dbi
+    @test get(txn, dbi, fastkey, "") == ""
+    @test get(txn, dbi, "stringkey", "") == ""
+end
+
+if get(ENV, "BENCH", "") == "true"
+    @info "Benchmarking write(::Environment) ..."
+    @btime write(env) do txn, dbi
+        put!(txn, dbi, fastkey, fastval);
+    end
+    @info "Benchmarking read(::Environment) with existing key ..."
+    @btime read(env) do txn, dbi
+        get(txn, dbi, fastkey, "");
+    end
+    write(env) do txn, dbi
+        delete!(txn, dbi, fastkey)
+    end    
+    @info "Benchmarking read(::Environment) with non-existing key ..."
+    @btime read(env) do txn, dbi
+        get(txn, dbi, fastkey, "");
+    end
+end
